7674488384c3d4ee8f7816d499cb65f36db38bc9
annavision21 <annavision3320@naver.com>
2024-06-21 05:50:52 +0900

    Declare Command for target2 and use libary of mips-cpu

--- Command/Target2.s
+++ Command/Target2.s
@@ -3,35 +3,35 @@ main:
     addiu   $t0,    $zero,  0x11        # $t0 = 0x00000011
     addiu   $t1,    $t0,    0x01        # $t1 = 0x00000012
     add     $t2,    $t1,    $t0         # $t2 = $t1 + $t0 = 0x00000023
-    addu    $t3,    $t2,    $t1         # $t3 = $t2 + $t1 = 0x00000034
+    addu    $t3,    $t2,    $t1         # $t3 = $t2 + $t1 = 0x00000035
     sub     $t2,    $t1,    $t0         # $t2 = $t1 - $t0 = 0x00000001
 
     and     $t2,    $t0,    $t1         # $t2 = $t0 & $t1 = 0x00000010
     andi    $t3,    $t0,    0x00000012  # $t3 = $t0 & 0x00000012 = 0x00000010
     or      $t2,    $t0,    $t1         # $t2 = $t0 | $t1 = 0x00000013
     ori     $t3,    $t0,    0x00000012  # $t3 = $t0 | 0x00000012 = 0x00000013
-    nor     $t2,    $t0,    $t1         # $t2 = $t0 ~| $t1 = 0xFFFFFFDC
-
-    sll     $t2,    $t0,    3           # $t2 = 0x88
-    srl     $t3,    $t2,    3           # $t3 = 0x11
-    sra     $t4,    $t2,    3           # $t4 = ?
+    nor     $t2,    $t0,    $t1         # $t2 = $t0 ~| $t1 = 0xFFFFFFEC
 
     slt     $t2,    $t0,    $t1         # t2 = 1
     slti    $t3,    $t0,    0x00000012  # t3 = 1
     sltu    $t4,    $t0,    $t1         # t4 = 1
 
-    bne     $t2,    $t3,    0x30       # if $t2 != $t3 goto 0x30
-    bne     $t0,    $t2,    0x30       # if $t0 != $t2 goto 0x30
+    sll     $t2,    $t0,    3           # $t2 = 0x88
+    srl     $t3,    $t2,    3           # $t3 = 0x11
+    sra     $t4,    $t2,    3           # $t4 = 0xF1
 
-0x30:
-    jal     0x40
-    jr      $t1
+    bne     $t2,    $t3,    test1       # if $t2 != $t3 goto test1(0x60)
+    bne     $t0,    $t2,    test1       # if $t0 != $t2 goto test1(0x60)
 
-0x40: 
-    nop
+test1:
+    jal     test2                       # Jump to test2(0x70)
+    jr      $ra                         # Jump to test3(0x80)
 
-0x80:
-    syscall
-    mfc0    $t0, $12
-    mtc0    $t0, $12
-    eret
+test2:
+    nop     
+
+test3:
+    syscall 
+    mfc0    $t0,    $12
+    mtc0    $t0,    $12
+    eret    
