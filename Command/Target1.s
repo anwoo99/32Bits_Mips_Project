e54c17e87ecd492be6d18879c864c17a23bb6c1c
annavision21 <annavision3320@naver.com>
2024-06-20 22:23:58 +0900

    Revise Target1.hex

--- Command/Target1.s
+++ Command/Target1.s
@@ -1,6 +1,7 @@
 main:
     addi    $t0,    $zero,  10      # $t0 = 10
     addi    $t1,    $zero,  0x1000  # $t2 = 0x1000
+    beq     $t0,    $t1,    end
     sw      $t0,    0($t1)          # Memory[0x1000] = 10
     lw      $t2,    0($t1)          # $t2 = Memory[$t1] (Memory[0x1000])
     beq     $t0,    $t2,    label   # if ($t0 == $t1) goto label
--- Command/Target2.s
+++ Command/Target2.s
@@ -1,24 +1,37 @@
 main:
-    # 산술
-    addi    $t0,    $zero,  0x11    # $t0 = 0x00000011
-    addi    $t1,    $t0,    0x01    # $t1 = 0x00000012
-    add     $t2,    $t1,    $t0     # $t2 = $t1 + $t0 = 0x00000023
-    sub     $t2,    $t1,    $t0     # $t2 = $t1 - $t0 = 0x00000001
-    
-    # 논리
-    and     $t4,    $t0,    $t1     # $t4 = $t0 & $t1 = 0x00000010
-    or      $t4,    $t0,    $t1     # $t4 = $t0 | $t1 = 0x00000013
-    nor     $t4,    $t0,    $t1     # $t4 = $t0 ~| $t1 = 0xFFFFFFDC
-    slt     $t4,    $t0,    $t1     # t4 = 1
+    addiu   $ra,    $zero,  0x80        # $rs = 0x00000080
+    addiu   $t0,    $zero,  0x11        # $t0 = 0x00000011
+    addiu   $t1,    $t0,    0x01        # $t1 = 0x00000012
+    add     $t2,    $t1,    $t0         # $t2 = $t1 + $t0 = 0x00000023
+    addu    $t3,    $t2,    $t1         # $t3 = $t2 + $t1 = 0x00000034
+    sub     $t2,    $t1,    $t0         # $t2 = $t1 - $t0 = 0x00000001
 
-    # 쉬프트
-    sll     $t5,    $t0,    3       # $t5 = 0x88
-    srl     $t6,    $t5     3       # $t6 = 0x11     
+    and     $t2,    $t0,    $t1         # $t2 = $t0 & $t1 = 0x00000010
+    andi    $t3,    $t0,    0x00000012  # $t3 = $t0 & 0x00000012 = 0x00000010
+    or      $t2,    $t0,    $t1         # $t2 = $t0 | $t1 = 0x00000013
+    ori     $t3,    $t0,    0x00000012  # $t3 = $t0 | 0x00000012 = 0x00000013
+    nor     $t2,    $t0,    $t1         # $t2 = $t0 ~| $t1 = 0xFFFFFFDC
 
-    # 분기
-    bne     $t5,    $t6,    label    # if $t5 != $t6 label()[0x30]
-    jal     label
-    jr      $t5
+    sll     $t2,    $t0,    3           # $t2 = 0x88
+    srl     $t3,    $t2,    3           # $t3 = 0x11
+    sra     $t4,    $t2,    3           # $t4 = ?
 
-label:
+    slt     $t2,    $t0,    $t1         # t2 = 1
+    slti    $t3,    $t0,    0x00000012  # t3 = 1
+    sltu    $t4,    $t0,    $t1         # t4 = 1
+
+    bne     $t2,    $t3,    0x30       # if $t2 != $t3 goto 0x30
+    bne     $t0,    $t2,    0x30       # if $t0 != $t2 goto 0x30
+
+0x30:
+    jal     0x40
+    jr      $t1
+
+0x40: 
     nop
+
+0x80:
+    syscall
+    mfc0    $t0, $12
+    mtc0    $t0, $12
+    eret
