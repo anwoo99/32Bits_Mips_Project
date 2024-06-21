808ad427c74d5cca87b8d1eaeb612f0fa14aba06
annavision21 <annavision3320@naver.com>
2024-06-21 16:27:06 +0900

    Revise Command and circuit

--- Command/Target1.s
+++ Command/Target1.s
@@ -1,12 +1,16 @@
 main:
     addi    $t0,    $zero,  10      # $t0 = 10
     addi    $t1,    $zero,  0x1000  # $t2 = 0x1000
-    beq     $t0,    $t1,    end
+    beq     $t0,    $t1,    end     # No Branch
     sw      $t0,    0($t1)          # Memory[0x1000] = 10
-    lw      $t2,    0($t1)          # $t2 = Memory[$t1] (Memory[0x1000])
-    beq     $t0,    $t2,    label   # if ($t0 == $t1) goto label
+    lw      $t2,    0($t1)          # $t2 = Memory[$t1] = 10 (Memory[0x1000])
+    beq     $t0,    $t2,    label   # Branch to label(0x08)
 
+# 0x08
 label:
     add     $t4,    $t0,    $t1     # $t4 = $t0 + $t1
-    j       end
-end:    nop                         # end of program
+    j       end                     # Branch to end(0x0c)
+
+# 0x0c
+end:
+    nop                             # end of program
--- Command/Target2.s
+++ Command/Target2.s
@@ -1,36 +1,40 @@
 main:
-    addiu   $ra,    $zero,  0x80        # $rs = 0x00000080
+    # 0x00
     addiu   $t0,    $zero,  0x11        # $t0 = 0x00000011
     addiu   $t1,    $t0,    0x01        # $t1 = 0x00000012
     add     $t2,    $t1,    $t0         # $t2 = $t1 + $t0 = 0x00000023
     addu    $t3,    $t2,    $t1         # $t3 = $t2 + $t1 = 0x00000035
     sub     $t2,    $t1,    $t0         # $t2 = $t1 - $t0 = 0x00000001
 
+    # 0x07
     and     $t2,    $t0,    $t1         # $t2 = $t0 & $t1 = 0x00000010
     andi    $t3,    $t0,    0x00000012  # $t3 = $t0 & 0x00000012 = 0x00000010
     or      $t2,    $t0,    $t1         # $t2 = $t0 | $t1 = 0x00000013
     ori     $t3,    $t0,    0x00000012  # $t3 = $t0 | 0x00000012 = 0x00000013
     nor     $t2,    $t0,    $t1         # $t2 = $t0 ~| $t1 = 0xFFFFFFEC
 
+    # 0x0c
     slt     $t2,    $t0,    $t1         # t2 = 1
     slti    $t3,    $t0,    0x00000012  # t3 = 1
     sltu    $t4,    $t0,    $t1         # t4 = 1
 
-    sll     $t2,    $t0,    3           # $t2 = 0x88
-    srl     $t3,    $t2,    3           # $t3 = 0x11
-    sra     $t4,    $t2,    3           # $t4 = 0xF1
+    # 0x0f
+    sll     $t2,    $t0,    0x1b        # $t2 = 0x88000000
+    srl     $t3,    $t2,    0x1b        # $t3 = 0x00000011
+    sra     $t4,    $t2,    0x1b        # $t4 = 0xFFFFFFF1
 
-    bne     $t2,    $t3,    test1       # if $t2 != $t3 goto test1(0x60)
-    bne     $t0,    $t2,    test1       # if $t0 != $t2 goto test1(0x60)
+    #0x12
+    bne     $t0,    $t3,    test1       # if $t2 != $t3 goto test1(0x1c) => No Branch
+    bne     $t0,    $t2,    test1       # if $t0 != $t2 goto test1(0x1c) => Start to Branch
 
-test1:
-    jal     test2                       # Jump to test2(0x70)
-    jr      $ra                         # Jump to test3(0x80)
+test1:                                  # 0x1c
+    jal     test2                       # Jump to test2(0x20)
+    
 
-test2:
-    nop     
+test2:                                  # 0x20
+    jr      $ra                         # return to  test1(0x1d)
 
-test3:
+test3:                                  # 0x24
     syscall 
     mfc0    $t0,    $12
     mtc0    $t0,    $12
